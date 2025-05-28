f78d44677a8f284884a9e63704ecc5174fa028be
Lathindu Pramuditha <alpdetails@gmail.com>
2025-05-28 18:33:32 +0530

    KAN-14: Update frontend/src/store/userSlice.js

--- frontend/src/store/userSlice.js
+++ frontend/src/store/userSlice.js
@@ -1,19 +1,4 @@
-import { createSlice } from '@reduxjs/toolkit';
-
-const initialState = {
-  notificationsEnabled: true,
-  notificationTime: new Date(Date.now()),
-};
-
-const userSlice = createSlice({
-  name: 'user',
-  initialState,
-  reducers: {
-    updateNotificationSettings(state, action) {
-      return { ...state, ...action.payload };  
-    },
-  },
-});
-
-export const { updateNotificationSettings } = userSlice.actions;
-export default userSlice.reducer;
+// Add this action:
+updateNotificationToken(state, action) {
+  state.notificationToken = action.payload;
+},
