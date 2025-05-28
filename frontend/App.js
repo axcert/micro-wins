ec2004a42204216fd77991ebe72d5854fc7493aa
Lathindu Pramuditha <alpdetails@gmail.com>
2025-05-28 18:22:10 +0530

    KAN-20: Update frontend/App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Provider } from 'react-redux'; // Redux Provider
-import { NavigationContainer } from '@react-navigation/native';
+import React, { useEffect } from 'react';
+import { Provider } from 'react-redux';
+import { NavigationContainer } from '@react-navigation/native'; 
 import { createStackNavigator } from '@react-navigation/stack';
 import SplashScreen from './src/screens/SplashScreen';
 import AuthStack from './src/navigation/AuthStack';
@@ -11,25 +11,20 @@ import PreviewSteps from './src/screens/PreviewSteps';
 import OnboardingScreen from './src/screens/OnboardingScreen';
 import SettingsScreen from './src/screens/SettingsScreen';
 
-
+import { registerForPushNotificationsAsync } from './src/utils/notifications';
 
 const Stack = createStackNavigator();
 
 const App = () => {
+  useEffect(() => {
+    registerForPushNotificationsAsync();
+  }, []);
+
   return (
     <Provider store={store}>
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Splash" component={SplashScreen} />
-        <Stack.Screen name="Auth" component={AuthStack} />
-        <Stack.Screen name="App" component={HomeScreen} />
-        <Stack.Screen name="CreateGoal" component={CreateGoal} />
-        <Stack.Screen name="PreviewSteps" component={PreviewSteps} />
-        <Stack.Screen name="Onboarding" component={OnboardingScreen} />
-        <Stack.Screen name="Settings" component={SettingsScreen} />
-          
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        {/* Navigation setup... */}
+      </NavigationContainer>
     </Provider>
   );
 };
