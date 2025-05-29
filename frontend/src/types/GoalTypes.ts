c551dc8411a19d851d11173756515d16753a2ba9
Lathindu Pramuditha <alpdetails@gmail.com>
2025-05-29 14:15:26 +0530

    KAN-18: Add frontend/src/types/GoalTypes.ts

---
+++ frontend/src/types/GoalTypes.ts
@@ -0,0 +1,12 @@
+export interface GoalData {
+  title: string;
+  description: string;
+  category: string;
+  targetDays: number;
+}
+
+export interface MicroStep {
+  id: string;
+  title: string;
+  description: string;
+}
