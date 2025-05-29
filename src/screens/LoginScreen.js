023223bc2360d7bde613eeacf30de0d844197399
Lathindu Pramuditha <alpdetails@gmail.com>
2025-05-29 12:48:00 +0530

    KAN-9: Add src/screens/LoginScreen.js

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loginAsync } from '../redux/authSlice';
+
+const LoginScreen = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector((state) => state.auth.isLoading);
+  const error = useSelector((state) => state.auth.error);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    dispatch(loginAsync({ email, password }));
+  };
+
+  return (
+    // JSX for login form
+    // Render email and password fields 
+    // Show loading spinner when isLoading is true
+    // Display error message if error exists
+    // Call handleLogin on form submit
+  );
+};
+
+export default LoginScreen;
